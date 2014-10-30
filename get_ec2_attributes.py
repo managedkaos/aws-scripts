20c76cd7c18e29a7ed9f7691f1e489015a0146a2
Michael <michael@managedkaos.com>
2014-10-29 22:35:18 -0700

    ec2_connection -> console
    Changed ec2_connection to console to follow the format of the other
    scripts.

--- get_ec2_attributes.py
+++ get_ec2_attributes.py
@@ -6,16 +6,16 @@ from boto import ec2
 
 printer = pprint.PrettyPrinter(indent=4)
 
-ec2_connection = boto.ec2.EC2Connection()
+console = boto.ec2.EC2Connection()
 
 # get reservations to look at instances
-reservations = ec2_connection.get_all_instances()
+reservations = console.get_all_instances()
 
 # get volumes
-volumes = ec2_connection.get_all_volumes()
+volumes = console.get_all_volumes()
 
 # get snapshots
-snapshots = ec2_connection.get_all_snapshots()
+snapshots = console.get_all_snapshots()
 
 print "____Instance____"
 for reservation in reservations:
